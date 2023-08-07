b242704a5a22a44a94131b173808908918d3868a
Tony Han <tony.han@microchip.com>
2023-08-07 16:14:38 +0800

    drivers: clk: sama7g54: not using UTMI driver for sama7g5
    Signed-off-by: Tony Han <tony.han@microchip.com>

--- core/drivers/clk/sam/sub.mk
+++ core/drivers/clk/sam/sub.mk
@@ -1,10 +1,11 @@
 global-incdirs-y += .
 
 at91-common = at91_sckc.c at91_main.c at91_pmc.c
-at91-common += at91_utmi.c at91_master.c
+at91-common += at91_master.c
 at91-common += at91_programmable.c at91_system.c at91_peripheral.c
 at91-common += at91_generated.c
 
+srcs-$(CFG_DRIVERS_SAMA5D2_CLK) += at91_utmi.c
 srcs-$(CFG_DRIVERS_SAMA5D2_CLK) += at91_pll.c at91_plldiv.c
 srcs-$(CFG_DRIVERS_SAMA5D2_CLK) += at91_h32mx.c at91_usb.c
 srcs-$(CFG_DRIVERS_SAMA5D2_CLK) += at91_i2s_mux.c at91_audio_pll.c
